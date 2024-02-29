b7609cf13613d752ff27a52b631ceb3884e9cb96
kiwi334334 <99292273+kiwi334334@users.noreply.github.com>
2024-02-29 15:50:56 +0000

    modified:   src/routes/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,63 @@
 <script lang="ts">
-   let count = $state(0);
-   let arrayWithAllMessages = $state([
-    'Hello World',
-    'Why Would You Do This',
-    'How Dare You!',
-    'Who are you?',
-    'AlphaSONIC!!!',
-    'Luke I Am Your Father',
-    'Hello There',
-    'Subway Eat Fresh',
-    'NOOOOOOOOOOOOOOOOOOOOOOOOOOOO!',
-    'In Response: Counter Spell'
-   ]);
+	let count = $state(0);
+	let leftLimit = $state(0);
+	let righLimit = $state(1);
+	let arrayWithAllMessages = $state([
+		'Hello World',
+		'Why Would You Do This',
+		'How Dare You!',
+		'Who are you?',
+		'AlphaSONIC!!!',
+		'Luke I Am Your Father',
+		'Hello There',
+		'Subway Eat Fresh',
+		'NOOOOOOOOOOOOOOOOOOOOOOOOOOOO!',
+		'In Response: Counter Spell'
+	]);
 </script>
 
 <div>
-    <button onclick={() => {count += 1; if(count > 9) {count = 0 }}}>{arrayWithAllMessages[count]}</button>
+	<button
+		on:click={() => {
+			leftLimit += 1;
+		}}>Increase Left Character Limit</button
+	>
+	<button
+		on:click={() => {
+			leftLimit -= 1;
+		}}>Decrease Left Character Limit</button
+	>
+	<br />
+	<button
+		on:click={() => {
+			righLimit += 1;
+		}}>Increase Right Character Limit</button
+	>
+	<button
+		on:click={() => {
+			righLimit -= 1;
+		}}>Decrease Right Character Limit</button
+	>
+    <br>
+	<button
+		onclick={() => {
+			count += 1;
+			if (count > 9) {
+				count = 0;
+			}
+		}}>Cycle Messages</button
+	>
+	<h1>
+		the characters in the string between {leftLimit}-{righLimit} are
+		<span>{arrayWithAllMessages[count].substring(leftLimit, righLimit)}</span>
+	</h1>
 </div>
 
 <style>
-    :root {
-        background: skyblue;
-    }
+	:root {
+		background: skyblue;
+	}
+	span {
+		color: blueviolet;
+	}
 </style>
