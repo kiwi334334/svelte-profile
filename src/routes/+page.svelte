c3c5080a49827d8930fc7cca7e8e535e9a7e5e90
kiwi334334 <99292273+kiwi334334@users.noreply.github.com>
2024-02-29 04:26:24 +0000

    modified:   src/routes/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
    let count = $state(0);
+   let arrayWithAllMessages = $state([
+    'Hello World',
+    'Why Would You Do This',
+    'How Dare You!',
+    'Who are you?',
+    'AlphaSONIC!!!',
+    'Luke I Am Your Father',
+    'Hello There',
+    'Subway Eat Fresh',
+    'NOOOOOOOOOOOOOOOOOOOOOOOOOOOO!',
+    'In Response: Counter Spell'
+   ])
 </script>
 
 <div>
-    <input type="number" bind:value={count}>
-    <h1>{count}</h1>
+    <button onclick={() => {count = Math.floor(Math.random() * 10)}}>{arrayWithAllMessages[count]}</button>
 </div>
+
+<style>
+
+</style>
